8d34ce3aa70019f0b414acbe3ede7f2ea12a89fe
gokuldastk <gokuldastk121@gmail.com>
2025-12-15 19:31:24 +0530

    Initial Commit

---
+++ NasaAPODApp/NasaAPODAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NasaAPODAppApp.swift
+//  NasaAPODApp
+//
+//  Created by Gokul on 15/12/25.
+//
+
+import SwiftUI
+
+@main
+struct NasaAPODAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NasaAPODAppTests/NasaAPODAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  NasaAPODAppTests.swift
+//  NasaAPODAppTests
+//
+//  Created by Gokul on 15/12/25.
+//
+
+import Testing
+@testable import NasaAPODApp
+
+struct NasaAPODAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
